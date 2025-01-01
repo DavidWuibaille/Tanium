7bf40f95aea082d1a312bf01f7ed4ee6667fbe26
David Wuibaille <david.wuibaille@outlook.fr>
2025-01-01 21:23:50 +0100

    Update Customer-ext.ps1

--- Provision/Customer-ext.ps1
+++ Provision/Customer-ext.ps1
@@ -7,6 +7,12 @@ $destination = "C:\Windows\Temp\GoogleChromeStandaloneEnterprise64.msi"
 $appName = "Google Chrome"
 TaniumDownloadAndInstallMsi -Url $url -Destination $destination -AppName $appName
 
+$url = "https://nas.wuibaille.fr/DML/7zip/7z1900-x64.msi"
+$destination = "C:\Windows\Temp\7z1900-x64.msi"
+$appName = "7z"
+TaniumDownloadAndInstallMsi -Url $url -Destination $destination -AppName $appName
+
+
 $computerInfo = Get-ComputerInfoFromAPI -WebServiceUrl "http://epm2024.monlab.lan:12176/GetName"
 $computerName = $computerInfo.Computername
 $postype      = $computerInfo.Postype
