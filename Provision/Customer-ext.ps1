ebf78b92e150551eaf8699a4bd6eac79105bbc6a
David Wuibaille <david.wuibaille@outlook.fr>
2025-01-06 10:18:16 +0100

    Update Customer-ext.ps1

--- Provision/Customer-ext.ps1
+++ Provision/Customer-ext.ps1
@@ -3,15 +3,15 @@ Import-Module C:\_T\TaniumOSD
 Import-Module C:\_T\TaniumClient
 
 Set-OSDProgressDisplay -Message "Applications"
-$url = "https://nas.wuibaille.fr/DML/Chrome/googlechromestandaloneenterprise64.msi"
-$destination = "C:\Windows\Temp\GoogleChromeStandaloneEnterprise64.msi"
-$appName = "Google Chrome"
-TaniumDownloadAndInstallMsi -Url $url -Destination $destination -AppName $appName
 
-$url = "https://nas.wuibaille.fr/DML/7zip/7z1900-x64.msi"
-$destination = "C:\Windows\Temp\7z1900-x64.msi"
-$appName = "7z"
-TaniumDownloadAndInstallMsi -Url $url -Destination $destination -AppName $appName
+# Installation de Google Chrome
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidWuibaille/Packaging/main/DML/chrome.ps1" -UseBasicParsing).Content
+
+# Installation de 7-Zip
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidWuibaille/Packaging/main/DML/7zip.ps1" -UseBasicParsing).Content
+
+# Installation de Adobe Acrobat DC
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidWuibaille/Packaging/main/DML/acrobatdc.ps1" -UseBasicParsing).Content
 
 Set-OSDProgressDisplay -Message "Web Service"
 $computerInfo = Get-ComputerInfoFromAPI -WebServiceUrl "http://epm2024.monlab.lan:12176/GetName"
