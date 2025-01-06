b3a3365165c543a3c3d6169a09ed3227aa176e16
David Wuibaille <david.wuibaille@outlook.fr>
2025-01-06 15:08:11 +0100

    Update Customer-PE-ext.ps1

--- Provision/Customer-PE-ext.ps1
+++ Provision/Customer-PE-ext.ps1
@@ -3,7 +3,7 @@ Import-Module C:\_T\TaniumOSD
 Import-Module C:\_T\TaniumClient
 
 $macaddress = Get-WmiObject Win32_NetworkAdapter | Where-Object { $_.NetConnectionStatus -eq 2 } | Select-Object -ExpandProperty MACAddress
-$macaddress = $macAddresses.replace(":", "").replace("-", "") # Nettoyer le format MAC
+$macaddress = $macaddress.Replace(":", "").Replace("-", "")
 
 $computerInfo = Get-ComputerInfoFromAPI -WebServiceUrl "http://epm2024.monlab.lan:12176/GetName"
 $computerName = $computerInfo.Computername
