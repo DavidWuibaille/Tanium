751f2eacc1ebc2db78d289569f0bbbb7b8fd5b8f
David Wuibaille <david.wuibaille@outlook.fr>
2025-01-06 10:42:03 +0100

    Update Customer-ext.ps1

--- Provision/Customer-ext.ps1
+++ Provision/Customer-ext.ps1
@@ -2,6 +2,7 @@
 Import-Module C:\_T\TaniumOSD
 Import-Module C:\_T\TaniumClient
 $macaddress = (Get-NetAdapter | Where-Object Status -eq 'Up').MacAddress -replace ":", ""
+$computername = (Get-ComputerInfo).CsName
 
 Set-OSDProgressDisplay -Message "Applications"
 
@@ -11,12 +12,12 @@ Invoke-RestMethod -Uri "http://epm2024.monlab.lan:12176/SaveInfo?macaddress=$mac
 Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidWuibaille/Packaging/main/DML/chrome.ps1" -UseBasicParsing).Content
 
 # Installation de 7-Zip
-$info = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $(Get-ComputerInfo).CsName - Installation 7zip"
+$info = "$computername - Installation 7zip"
 Invoke-RestMethod -Uri "http://epm2024.monlab.lan:12176/SaveInfo?macaddress=$macaddress&info=$info" -Method Post
 Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidWuibaille/Packaging/main/DML/7zip.ps1" -UseBasicParsing).Content
 
 # Installation de Adobe Acrobat DC
-$info = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $(Get-ComputerInfo).CsName - Installation Acrobat DC"
+$info = "$computername - Installation Acrobat DC"
 Invoke-RestMethod -Uri "http://epm2024.monlab.lan:12176/SaveInfo?macaddress=$macaddress&info=$info" -Method Post
 Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidWuibaille/Packaging/main/DML/acrobatdc.ps1" -UseBasicParsing).Content
 
@@ -28,23 +29,23 @@ $setkeyboard  = $computerInfo.SetKeyboard
 Log-Message "API : $computerName"  
 Log-Message "API : $postype"   
 Log-Message "API : $setkeyboard" 
-$info = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $(Get-ComputerInfo).CsName - API $computerName $postype $setkeyboard"
+$info = "$computername - API $computerName $postype $setkeyboard"
 Invoke-RestMethod -Uri "http://epm2024.monlab.lan:12176/SaveInfo?macaddress=$macaddress&info=$info" -Method Post
 
 # Set the environment variable POSTYPE persistently for the System
 [Environment]::SetEnvironmentVariable("POSTYPE",  $postype, [EnvironmentVariableTarget]::Machine)
 
 Set-OSDProgressDisplay -Message "Drivers"
-$info = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $(Get-ComputerInfo).CsName - Install Drivers"
+$info = "$computername - Install Drivers"
 Invoke-RestMethod -Uri "http://epm2024.monlab.lan:12176/SaveInfo?macaddress=$macaddress&info=$info" -Method Post
 InstallDrivers
 
 Set-OSDProgressDisplay -Message "Domain"
-$info = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $(Get-ComputerInfo).CsName - Integrate domain"
+$info = "$computername - Integrate domain"
 Invoke-RestMethod -Uri "http://epm2024.monlab.lan:12176/SaveInfo?macaddress=$macaddress&info=$info" -Method Post
 SetDns -DnsServers @("192.168.0.240", "192.168.0.3")
 Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidWuibaille/Tanium/main/Provision/joindomain.ps1" -UseBasicParsing).Content
 
-$info = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $(Get-ComputerInfo).CsName - END"
+$info = "$computername - END"
 Invoke-RestMethod -Uri "http://epm2024.monlab.lan:12176/SaveInfo?macaddress=$macaddress&info=$info" -Method Post
 Set-OSDProgressDisplay -Message "End"
